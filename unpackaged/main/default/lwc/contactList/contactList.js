4d2fe5d76d278a48987cea9f2cd080a4a56087f1
= <adamnila@aenila.com>
2021-09-30 17:10:36 -0400

    chaging ldsUtils import

--- unpackaged/main/default/lwc/contactList/contactList.js
+++ unpackaged/main/default/lwc/contactList/contactList.js
@@ -1,6 +1,6 @@
 import { LightningElement, wire } from 'lwc';
 import getContacts from '@salesforce/apex/ContactController.getContacts';
-import { reduceErrors } from 'c/ldsUtils';
+import { reduceErrors } from './ldsUtils';
 import FNAME from '@salesforce/schema/Contact.FirstName';
 import LNAME from '@salesforce/schema/Contact.LastName';
 import EML from '@salesforce/schema/Contact.Email';
